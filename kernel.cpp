8db7212a33d4f4c9b78a915eb7c4e582571a7bb5
Ansh-droid-glitch <anshsingh131014@gmail.com>
2026-03-24 10:55:01 +0530

    Yap Yap

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "keyboard.h"
 
 static uint16_t* vga = (uint16_t*)0xB8000;
 
@@ -8,9 +9,7 @@ static int cursor_y = 0;
 char input[256];
 int input_pos = 0;
 
-int shift_pressed = 0;
-
-void putchar(char c)
+extern "C" void putchar(char c)
 {
     if (c == '\n') {
         cursor_x = 0;
@@ -33,68 +32,21 @@ void putchar(char c)
     }
 }
 
-void print(const char* s)
+extern "C" void print(const char* s)
 {
     for (int i = 0; s[i]; i++)
         putchar(s[i]);
 }
 
-static inline uint8_t inb(uint16_t port)
+extern "C" void clear_screen()
 {
-    uint8_t r;
-    asm volatile("inb %1, %0" : "=a"(r) : "Nd"(port));
-    return r;
+    for (int i = 0; i < 80 * 25; i++)
+        vga[i] = (0x07 << 8) | ' ';
+    cursor_x = 0;
+    cursor_y = 0;
 }
 
-static const char keymap[128] = {
-0,27,'1','2','3','4','5','6','7','8','9','0','-','=', '\b',
-'\t','q','w','e','r','t','y','u','i','o','p','[',']','\n',0,
-'a','s','d','f','g','h','j','k','l',';','\'','`',0,'\\',
-'z','x','c','v','b','n','m',',','.','/',0,'*',0,' ',
-};
-
-static const char keymap_shift[128] = {
-0,27,'!','@','#','$','%','^','&','*','(',')','_','+', '\b',
-'\t','Q','W','E','R','T','Y','U','I','O','P','{','}','\n',0,
-'A','S','D','F','G','H','J','K','L',':','"','~',0,'|',
-'Z','X','C','V','B','N','M','<','>','?',0,'*',0,' ',
-};
-
-char keyboard_getchar()
-{
-    uint8_t sc;
-
-    while (1)
-    {
-        if (inb(0x64) & 1)
-        {
-            sc = inb(0x60);
-
-            if (sc == 0x2A || sc == 0x36) {
-                shift_pressed = 1;
-                continue;
-            }
-
-            if (sc == 0xAA || sc == 0xB6) {
-                shift_pressed = 0;
-                continue;
-            }
-
-            if (sc & 0x80)
-                continue;
-
-            if (sc < sizeof(keymap))
-            {
-                if (shift_pressed)
-                    return keymap_shift[sc];
-                else
-                    return keymap[sc];
-            }
-        }
-    }
-}
-
-int strncmp(const char* a, const char* b, int n)
+extern "C" int strncmp(const char* a, const char* b, int n)
 {
     for (int i = 0; i < n; i++)
         if (a[i] != b[i])
@@ -102,18 +54,22 @@ int strncmp(const char* a, const char* b, int n)
     return 0;
 }
 
-void run_command(char* cmd)
+extern "C" void run_command(char* cmd)
 {
     if (!strncmp(cmd, "print(\"", 7))
     {
         char* text = cmd + 7;
-
         while (*text && *text != '"') {
             putchar(*text);
             text++;
         }
-
         putchar('\n');
+        return;
+    }
+
+    if (!strncmp(cmd, "clear", 5))
+    {
+        clear_screen();
         return;
     }
 
---
+++ keyboard.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char keyboard_getchar();
+
+#ifdef __cplusplus
+}
+#endif
